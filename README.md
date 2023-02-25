3d628ff1bdcd51f6694545d5b001c5f062f4f9ee
reinerkp <kate.reiner1@gmail.com>
2023-02-25 13:53:49 -0700

    added inquirer prompt

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# READMEFiles
+# <j>
+
+## Description 
+j
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Needed Software
+j
+
+## Usage
+j
+
+## Credits
+Collaborators worked with: j
+Third Parties used: j
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = ({title, discription, intall, usage, collaborators,thirdparty}); => 
+const questions = ({title, description, install, usage, collaborators,thirdparty}) =>
 `# <${title}>
 
 ## Description 
-${discription}
+${description}
 
 ## Table of Contents (Optional)
 
@@ -18,7 +19,7 @@ ${discription}
 ${install}
 
 ## Usage
-${unsage}
+${usage}
 
 ## Credits
 Collaborators worked with: ${collaborators}
@@ -28,12 +29,51 @@ Third Parties used: ${thirdparty}
 
 The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
 `
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your projects title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a brief description of your project. Include a description of why you built it and any challenges you came across along the way.',
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'give instructions for how to properly install any software and run the application?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Give instructions for a user to be able to follow while using your application. You can use a formal that follows the WHEN and Then formatting.',
+    },
+    {
+      type: 'input',
+      name: 'collaborators',
+      message: 'Enter a full list of names of anyone whom you collobarated with.',
+    },
+    {
+      type: 'input',
+      name: 'thirdparty',
+      message: 'Enter a full list of Third-Party names you used for the project.',
+    },
+    // {
+    //     type: 'list',
+    //     name: 'license',
+    //     message: 'Which lecense did you choose',
+    //     choices: [MIT]
+    //   },
+  ])
+  .then((answers) => {
+    const readmePageContent = questions(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    fs.writeFile('README.md', readmePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
